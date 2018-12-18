ec8d0415856e19b8120726ad6364abb7c9aa8abd
Donna St. Louis <donna3@wharton.upenn.edu>
2018-12-18 18:30:56 -0500

    Feature: ability to parse sentence from POST request

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 from cyk_parser.cyk_tree import CYKParser
@@ -11,14 +11,22 @@ app.config.from_object(__name__)
 # enable CORS
 CORS(app)
 
-@app.route('/', methods=['GET'])
-@app.route('/parse', methods=['GET'])
-def parse():
+def get_tree(sentence):
     parser = CYKParser('cyk_parser/grammars/english.txt')
-    table = parser.generate_cyk_table("amy ate fish for dinner on tuesday")
+    table = parser.generate_cyk_table(sentence)
     root = parser.build_tree(table, 'S', row=len(table)-1, col=0)
+    if root is None:
+        print(f"Ungrammatical sentence: {sentence}")
     tree = Tree(root)
-    return jsonify(tree.serialize())
+    data = tree.serialize()
+    return jsonify(data)
+
+@app.route('/', methods=['GET', 'POST'])
+def parse():
+    sentence = 'fish swim in streams'
+    if request.method == 'POST':
+        sentence = str(request.get_json()['value'].lower())
+    return get_tree(sentence)
 
 
 if __name__ == '__main__':
